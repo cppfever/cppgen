c484701e07a5d81868c0c922ae7d89885931219c
dips42 <cppfever@gmail.com>
2019-06-23 16:41:42 +0700

    Enumerating works OK

--- cppgen.py
+++ cppgen.py
@@ -4,16 +4,26 @@ import typing
 index = clang.cindex.Index.create()
 tu = index.parse('SDL2/include/SDL_keycode.h', args=['-std=c++17'])
 
+enums = []
+enums_count = 0
 
 def find_enums(nodes: typing.Iterable[clang.cindex.Cursor]):
     for i in nodes:
     	if i.kind == clang.cindex.CursorKind.ENUM_DECL :
+    		global enums_count
+    		enums.append(('SomeEnum' + str(enums_count), []))
+    		enums_count = enums_count+1
     		find_enum_constants(i.get_children())
 
 def find_enum_constants(nodes: typing.Iterable[clang.cindex.Cursor]):
     for i in nodes:
     	if i.kind == clang.cindex.CursorKind.ENUM_CONSTANT_DECL :
-    		print(i.displayname)
+    		enums[-1][1].append(i.displayname)
 
 find_enums(tu.cursor.get_children())
-print("\nEnd of script")
+
+for i in enums:
+	print(i[1],'\n')
+		
+
+print("End of script")

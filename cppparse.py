9b59b90d4b50afb99152f4e18d254f0ca5b3dfe7
dips42 <cppfever@gmail.com>
2019-07-07 08:49:20 +0700

    Generator class

--- cppparse.py
+++ cppparse.py
@@ -10,6 +10,18 @@ enums = []
 new_enums = []
 enums_count = 0#for anonimouse enums(like C 'typedef enum')
 
+def print_enums():
+	for i in enums:
+		print(i)		
+
+def print_new_enums():
+	for i in new_enums:
+		print(i)		
+
+def compare_enums():
+	for i in new_enums:
+		print(i)		
+
 #Find clang.cindex.CursorKind.ENUM_DECL
 def find_enums(nodes: typing.Iterable[clang.cindex.Cursor]):
     for i in nodes:
@@ -31,27 +43,57 @@ def find_enum_constants(nodes: typing.Iterable[clang.cindex.Cursor]):
 #Find all anums
 find_enums(tu.cursor.get_children())	
 
-def upper_repl(match):
+def remove_underscore_capitalize_next(match):
 	return match.group(1).upper()
 
-def replace_prefix(src: str, prefix: str)->str:
+def replace_prefix_impl(src: str, prefix: str)->str:
 	prefix_length = len(prefix)
+	# Remove prefix and capitalize begin of result string
 	result =  src[prefix_length:].capitalize()
-	result = re.sub(r'_(.)', upper_repl, result)
+	# Remove all '_' sumbols and capitalize next symbol
+	result = re.sub(r'_(.)', remove_underscore_capitalize_next, result)
 	return result
 
-def replace_pattern(enum, pattern: str):
-	new_enums.append([])
+def replace_prefix(enum, pattern: str, new_name: str):
+	new_enums.append([new_name])
 	for i in enum[1]:
-		s = replace_prefix(i, pattern)
+		s = replace_prefix_impl(i, pattern)
 		new_enums[-1].append(s)
 
-replace_pattern(enums[2], 'SDL_SCANCODE_')
-replace_pattern(enums[3], 'SDLK_')
-replace_pattern(enums[4], 'KMOD_')
+replace_prefix(enums[2], 'SDL_SCANCODE_', 'ScanCode')
+replace_prefix(enums[3], 'SDLK_', 'KeyCode')
+replace_prefix(enums[4], 'KMOD_', 'KeyMode')
 
-for i in new_enums:
-	print(i)		
+#print_enums()
+#print_new_enums()
+
+def generate_new_enums():
+	for i in new_enums[1]:
+		new_enums[-1].append(s)
+
+print('End of script')
 
 
-print("End of script")
+# C++ generator
+
+class Generator:
+	class EmptyGenerator:
+		pass
+	empty = EmptyGenerator()
+	def __init__(self, *args):
+		self.list = args
+
+class TranslationUnit(Generator):
+	def __init__(self, file_name: str, *args):
+		Generator.__init__(self, args)
+		self.file_name = file_name
+
+class Enum(Generator):
+	def __init__(self, name: str, *args):
+		Generator.__init__(self, args)
+		self.file_name = file_name
+
+g = Generator(
+		Generator(),
+		TranslationUnit('keys.hpp', 
+			Generator()))
